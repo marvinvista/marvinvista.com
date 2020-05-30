b7261e65db7268b9858f576e181c0396657cafa3
Marvin Ryan Vista <marvin@marvinvista.com>
2020-05-30 17:40:44 +0800

    js

--- script/script.js
+++ script/script.js
@@ -3,19 +3,19 @@
     const annotationGroup = RoughNotation.annotationGroup;
     const $ = (t) => document.querySelector(t);
   
- export interface RoughAnnotationConfig {
-    type: RoughAnnotationType;
-    animate?: boolean; // defaults to true
-    animationDuration?: number; // defaulst to 1000ms
-    animationDelay?: number; // default = 0
-    color?: string; // defaults to currentColor
-    strokeWidth?: number; // default based on type
-    padding?: number; // defaults to 5px
+    // export interface RoughAnnotationConfig {
+    //   type: RoughAnnotationType;
+    //   animate?: boolean; // defaults to true
+    //   animationDuration?: number; // defaulst to 1000ms
+    //   animationDelay?: number; // default = 0
+    //   color?: string; // defaults to currentColor
+    //   strokeWidth?: number; // default based on type
+    //   padding?: number; // defaults to 5px
     // }
   
     {
       // top
-      const a1 = annotate($('h4'), { type: 'highlight', color: '#FFF176' });
+      const a1 = annotate($('h4'), { type: 'highlight', color: '#FFF176', strokeWidth: '2px'});
       const ag = annotationGroup([a1]);
       ag.show();
-    }
+    }})();
