10ab81fed7beab234eed16d8e92ad2ad6fe2c151
Marvin Ryan Vista <marvin@marvinvista.com>
2020-05-30 19:02:47 +0800

    js

--- script/script.js
+++ script/script.js
@@ -16,10 +16,15 @@
     {
       // top
       const a1 = annotate($('h4'), { type: 'highlight', color: '#FFF176'});
-      const a2 = annotate($('h6'), { type: 'box', color: '#FFF176'});
-      const a3 = annotate($('a'), { type: 'underline', color: '#3ecf8e', animationDuration: 500});
+      const a2 = annotate($('h6'), { type: 'box', color: '#FFF176', padding: 1});
+      const a3 = annotate($('a.link'), { type: 'underline', color: '#3ecf8e', animate: false, padding: 3});
+      const a4 = annotate($('a.email'), { type: 'underline', color: '#3ecf8e', animate: false, padding: 3});
+      const a5 = annotate($('a.twitter'), { type: 'underline', color: '#3ecf8e', animate: false, padding: 3});
+
     //   const ag = annotationGroup([a1, a2, a3]);
       a1.show();
-      a2.show();
+    //   a2.show();
       a3.show();
+      a4.show();
+      a5.show();    
     }})();
