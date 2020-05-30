e47bede6a538339bca486c1d6a18d570f63f0d3a
Marvin Ryan Vista <marvin@marvinvista.com>
2020-05-30 17:31:57 +0800

    js

---
+++ script/script.js
@@ -0,0 +1,21 @@
+(function () {
+    const annotate = RoughNotation.annotate;
+    const annotationGroup = RoughNotation.annotationGroup;
+    const $ = (t) => document.querySelector(t);
+  
+ export interface RoughAnnotationConfig {
+    type: RoughAnnotationType;
+    animate?: boolean; // defaults to true
+    animationDuration?: number; // defaulst to 1000ms
+    animationDelay?: number; // default = 0
+    color?: string; // defaults to currentColor
+    strokeWidth?: number; // default based on type
+    padding?: number; // defaults to 5px
+    // }
+  
+    {
+      // top
+      const a1 = annotate($('h4'), { type: 'highlight', color: '#FFF176' });
+      const ag = annotationGroup([a1]);
+      ag.show();
+    }
